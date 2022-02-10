6bc78485b30ffe47eb5398e7f48963706836ac7a
luizsaulo <luizsaulo@yahoo.com.br>
2022-02-10 20:01:13 -0300

    initializing a new complete project by adding a server side on firebase and the css reset properties to make it all clean to go

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAy1EsfbPaBZlfxvDfD1Eo6qkd0zMHayAI",
+    authDomain: "sistema-chamados-7cf43.firebaseapp.com",
+    projectId: "sistema-chamados-7cf43",
+    storageBucket: "sistema-chamados-7cf43.appspot.com",
+    messagingSenderId: "999507686250",
+    appId: "1:999507686250:web:ed81646cb89aa800a32b13",
+    measurementId: "G-V94SBM2GM0"
+  };
+  
+if(!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
