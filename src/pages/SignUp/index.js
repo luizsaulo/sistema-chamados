30261a3877f164f9400206248c9dd20e59d51674
luizsaulo <luizsaulo@yahoo.com.br>
2022-02-11 15:10:31 -0300

    creating registration page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,35 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '../../assets/logo.png';
 
 function SignUp() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert('Clicou')
+  }
+  
     return (
-      <div>
-        <h1>Tela de Cadastro</h1>
+      <div className='container-center'>
+        <div className='login'>
+          <div className='logo-area'>
+            <img src={logo} alt='login-area' />
+          </div>
+
+          <form onSubmit={handleSubmit}>
+            <h1>Criar uma conta</h1>
+            <input type='text' placeholder='Seu nome' value={ (e) => setNome(e.target.value)} />
+            <input type='text' placeholder='e-mail' value={email} onChange={ (e) => setEmail(e.target.value)} />
+            <input type='password' placeholder='******' value={password} onChange={ (e) => setPassword(e.target.value)} />
+            <button type='submit'>Cadastrar</button>
+          </form>
+
+          <Link to='/'>Já possui uma conta? Entre!</Link>
+        </div>
       </div>
     );
   }
