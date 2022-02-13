b6e7ae2acb577b2544fe41f834eee8cfcf475878
luizsaulo <luizsaulo@yahoo.com.br>
2022-02-13 16:17:54 -0300

    creating alerts with toastify

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,5 +1,6 @@
 import { useState, createContext, useEffect } from 'react';
 import firebase from '../services/firebaseConnection';
+import { toast } from 'react-toastify';
 
 export const AuthContext = createContext({});
 
@@ -46,11 +47,13 @@ function AuthProvider({ children }){
             setUser(data);
             storageUser(data);
             setLoadingAuth(false);
+            toast.success('Bem vindo de volta!');
 
         })
 
         .catch((error)=> {
             console.log(error);
+            toast.error('Ops, algo deu errado!');
             setLoadingAuth(false);
         })
     }
@@ -79,11 +82,13 @@ function AuthProvider({ children }){
                 setUser(data);
                 storageUser(data);
                 setLoadingAuth(false);
+                toast.success("Bem vindo à plataforma!");
 
             })
         })
         .catch((error)=>{
             console.log(error)
+            toast.error('Ops, algo deu errado!')
             setLoadingAuth(false);
         })
     }
