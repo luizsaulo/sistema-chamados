bbe615e48e3f97e34197621527768094cea31e83
luizsaulo <luizsaulo@yahoo.com.br>
2022-02-14 16:02:30 -0300

    creating the title component with its properties and style sheet

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,20 @@
 import { Switch } from 'react-router-dom';
 import Route from './Route';
+
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
-import DashBoard from '../pages/Dashboard';
+
 import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
 export default function Routes(){
     return(
         <Switch>
            <Route exact path="/" component={SignIn} />
            <Route exact path="/register" component={SignUp} />
+
            <Route exact path="/dashboard" component={Dashboard} isPrivate />
+           <Route exact path='/profile' component={Profile} isPrivate />
         </Switch>
     )
 }
