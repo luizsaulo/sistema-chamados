f12008eb3e4d5878fbc9e0d444ad05aef66dc30d
luizsaulo <luizsaulo@yahoo.com.br>
2022-02-16 16:23:48 -0300

    profile edition with all its properties and connections with the database

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -113,7 +113,9 @@ function AuthProvider({ children }){
             signUp,
             signOut,
             signIn,
-            loadingAuth
+            loadingAuth,
+            setUser,
+            storageUser
         }}
             >
             {children}
