7bef3b5bdeedc29d1ab0bd9c84f77ac03e34e414
luizsaulo <luizsaulo@yahoo.com.br>
2022-02-19 11:18:56 -0300

    implememnting the request registration properties, they are all working just fine

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -6,6 +6,7 @@ import firebase from '../../services/firebaseConnection';
 import Header from '../../components/Header';
 import Title from '../../components/Title';
 import { AuthContext } from '../../contexts/auth';
+import { toast } from 'react-toastify';
 
 import './new.css';
 import { FiPlusCircle } from 'react-icons/fi';
@@ -59,9 +60,29 @@ export default function New(){
 
     }, []);
 
-    function handleRegister(e){
+    async function handleRegister(e){
         e.preventDefault();
-        alert('teste')
+        
+        await firebase.firestore().collection('chamados')
+        .add({
+            created: new Date(),
+            cliente: customers[customerSelected].nomeFantasia,
+            clienteId: customers[customerSelected].id,
+            assunto: assunto,
+            status: status,
+            complemento: complemento,
+            userId: user.uid
+        })
+        .then(()=> {
+            toast.success('Chamado criado com sucesso!');
+            setComplemento('');
+            setCustomerSelected(0);
+        })
+        .catch((err)=> {
+            toast.error('Ops, erro ao tentar registrar, tente novamente mais tarde.')
+            console.log(err);
+        })
+
     }
 
     //Chamado quando troca o assunto
